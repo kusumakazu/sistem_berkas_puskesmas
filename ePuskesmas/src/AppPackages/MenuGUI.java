505eec9c15c40984ea55aa5f1b689f4f7c278856
kusumakazu <kusumakazu@users.noreply.github.com>
2019-11-15 11:23:20 +0700

    updated size

--- ePuskesmas/src/AppPackages/MenuGUI.java
+++ ePuskesmas/src/AppPackages/MenuGUI.java
@@ -33,11 +33,11 @@ public class MenuGUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1049, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 545, Short.MAX_VALUE)
         );
 
         pack();
