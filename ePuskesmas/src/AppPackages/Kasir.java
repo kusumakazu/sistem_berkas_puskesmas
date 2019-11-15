922c873b2e432b7f8668e4c56776a1fc3b2d74c2
kusumakazu <57720818+kusumakazu@users.noreply.github.com>
2019-11-15 19:10:10 +0700

    update new class
    added new class :
    - Apotek
    - Dokter
    - Kasir
    - Pasien

---
+++ ePuskesmas/src/AppPackages/Kasir.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AppPackages;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Kasir {
+    private double totalHarga;
+    private double itemHarga;
+    private double cetakAll;
+}
---
+++ ePuskesmas/src/AppPackages/Pasien.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AppPackages;
+
+import java.io.*;
+/**
+ *
+ * @author Administrator
+ */
+public class Pasien implements Serializable
+{     
+    private char namaPasien;
+    private char umurPasien;
+    private char jkPasien;
+    private String alamatPasien;
+    
+}
